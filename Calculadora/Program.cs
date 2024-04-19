6a77a50f17d58ca89fa43aba94080622dbf14dd7
gutsserrano <gutsserrano@gmail.com>
2024-04-18 21:33:32 -0300

    Adicionado escolha de operação e escolha de continuar ou não

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -5,30 +5,63 @@ double sub;
 double mult;
 double div;
 
-Console.WriteLine("Digite o primeiro valor: ");
-n1 = double.Parse(Console.ReadLine());
+int opcao = 0;
+int continuar = 0;
 
 do
 {
-    Console.WriteLine("Digite o segundo valor: ");
-    n2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o primeiro valor: ");
+    n1 = double.Parse(Console.ReadLine());
 
-    if (n2 == 0)
+    do
     {
-        Console.WriteLine("**N2 não pode ser 0 pois é impossível dividir por 0**\n");
+        Console.WriteLine("Digite o segundo valor: ");
+        n2 = double.Parse(Console.ReadLine());
+
+        if (n2 == 0)
+        {
+            Console.WriteLine("**N2 não pode ser 0 pois é impossível dividir por 0**\n");
+        }
+    } while (n2 == 0);
+
+    do
+    {
+        Console.WriteLine("\nSelecione qual operação fazer:");
+        Console.WriteLine("1 - Adição");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+
+        opcao = int.Parse(Console.ReadLine());
+    } while (opcao < 1 || opcao > 4);
+
+    if (opcao == 1)
+    {
+        adc = n1 + n2;
+        Console.WriteLine($"\n{n1} + {n2} = {adc}");
     }
-} while (n2 == 0);
+    else if (opcao == 2)
+    {
+        sub = n1 - n2;
+        Console.WriteLine($"\n{n1} - {n2} = {sub}");
+    }
+    else if (opcao == 3)
+    {
+        mult = n1 * n2;
+        Console.WriteLine($"\n{n1} * {n2} = {mult}");
+    }
+    else
+    {
+        div = n1 / n2;
+        Console.WriteLine($"\n{n1} / {n2} = {div}\n");
+    }
 
-adc = n1 + n2;
-sub = n1 - n2;
-mult = n1 * n2;
-div = n1 / n2;
+    do
+    {
+        Console.WriteLine("\nDeseja continuar?");
+        Console.WriteLine("1 - Sim");
+        Console.WriteLine("2 - Não");
+        continuar = int.Parse(Console.ReadLine());
+    }while(continuar < 0 || continuar > 2);
 
-Console.WriteLine($"{n1} + {n2} = {adc}");
-Console.WriteLine($"{n1} - {n2} = {sub}");
-Console.WriteLine($"{n1} * {n2} = {mult}");
-Console.WriteLine($"{n1} / {n2} = {div}\n");
-
-
-Console.WriteLine("Pressione qualquer tecla para sair...");
-Console.ReadKey();
+} while (continuar == 1);
