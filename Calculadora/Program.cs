5d8f4b7f6eafbcb22b72bc8cd29c7ac275a54322
gutsserrano <gutsserrano@gmail.com>
2024-04-18 21:16:37 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,34 @@
+﻿double n1, n2;
+
+double adc;
+double sub;
+double mult;
+double div;
+
+Console.WriteLine("Digite o primeiro valor: ");
+n1 = double.Parse(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Digite o segundo valor: ");
+    n2 = double.Parse(Console.ReadLine());
+
+    if (n2 == 0)
+    {
+        Console.WriteLine("**N2 não pode ser 0 pois é impossível dividir por 0**\n");
+    }
+} while (n2 == 0);
+
+adc = n1 + n2;
+sub = n1 - n2;
+mult = n1 * n2;
+div = n1 / n2;
+
+Console.WriteLine($"{n1} + {n2} = {adc}");
+Console.WriteLine($"{n1} - {n2} = {sub}");
+Console.WriteLine($"{n1} * {n2} = {mult}");
+Console.WriteLine($"{n1} / {n2} = {div}\n");
+
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
